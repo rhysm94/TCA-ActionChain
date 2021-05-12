32aa024ee98f921699c8369205b17cbc0bf977f9
Rhys Morgan <rhysm@me.com>
2021-05-12 14:34:29 +0100

    Initial Commit

---
+++ TCA-ActionChain/TCA_ActionChainApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCA_ActionChainApp.swift
+//  TCA-ActionChain
+//
+//  Created by Rhys Morgan on 12/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct TCA_ActionChainApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
